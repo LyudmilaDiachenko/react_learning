5ef8c7610f9cf97104431a37aa92180b13a8f510
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-15 13:13:00 +0300

    chp

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-ReactDOM.render(<div>
-    <h1>Hi</h1>
-    <h2>Hello</h2>
-    </div>, document.getElementById("app"))
+ReactDOM.render(React.createElement('input', {
+    placeholder: "Help text",
+    onClick: () => console.log('Click'),
+}), document.getElementById("app"))
