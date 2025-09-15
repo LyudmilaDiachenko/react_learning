1cb955092b4f9a7a60db12d1be5cc3a26531083b
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-15 15:03:17 +0300

    i react & i react-dom

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-ReactDOM.render(React.createElement('input', {
-    placeholder: "Help text",
-    onClick: () => console.log('Click'),
-}), document.getElementById("app"))
+// ReactDOM.render(React.createElement('input', {
+//     placeholder: "Help text",
+//     onClick: () => console.log('Click'),
+// }), document.getElementById("app"))
+
+
+const titleH1 = "Title H1"
+const placeholderTxt = "Help text!"
+const inputClick = () => console.log('Click')
+const mouseOver = () => console.log("Mouse over")
+
+const elements = (<div className="boxName">
+        <h1>{titleH1}</h1>
+        <input placeholder={placeholderTxt} onClick={inputClick} onMouseOver={mouseOver}/>
+        <p>{placeholderTxt == "Help text" ? "Yes" : "No"}</p>
+</div>) 
+const app = document.getElementById("app")
+
+ReactDOM.render(elements, app)
