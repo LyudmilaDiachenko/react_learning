60b2ee041209c12b46f8cc1d850c2f2d3b9ec6e6
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-16 11:54:00 +0300

    function components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from "react"
 import * as ReactDOMClient from "react-dom/client"
 
-// ReactDOM.render(React.createElement('input', {
-//     placeholder: "Help text",
-//     onClick: () => console.log('Click'),
-// }), document.getElementById("app"))
-
-
 const titleH1 = "Title H1"
 const placeholderTxt = "Help text!"
 const inputClick = () => console.log('Click')
 const mouseOver = () => console.log("Mouse over")
 
-const elements = (<div className="boxName">
+function Header(){
+  return(
+    <header>
+      Шапка сауйту
+    </header>
+  )
+}
+
+function App() {
+  return (<div className="boxName">
+    <Header />
+    <Header />
+    <Header />
         <h1>{titleH1}</h1>
         <input placeholder={placeholderTxt} onClick={inputClick} onMouseOver={mouseOver}/>
         <p>{placeholderTxt === "Help text" ? "Yes" : "No"}</p>
-</div>) 
+</div>)
+}
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
-app.render(elements)
+app.render(<App />)
