27b26c41cd49911a85f75840aff9ac5108764676
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-18 13:54:41 +0300

    add user

---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { CgCloseO } from "react-icons/cg";
+import { IoBuildSharp } from "react-icons/io5";
+
+class  User extends React.Component {
+    render(){
+        return(
+            <div className="users">
+                <CgCloseO className="delete-icon" />
+                <IoBuildSharp className="edit-icon" />
+                <h3>{this.props.user.firstname} {this.props.user.lastname}</h3>
+                <p>{this.props.user.bio}</p>
+                <b>{this.props.user.isHappy ?  "Щасливий:)" : "Недуже щасливий:("}</b>
+            </div>
+        )
+    }
+}
+
+export default User
