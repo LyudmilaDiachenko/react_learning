3974bf27fea19ff4a19becfb959f011f21ba9344
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-19 16:19:18 +0300

    value, validate

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,37 +1,49 @@
 import React from "react"
 
 class AddUser extends React.Component {
+    userAdd = {}
     constructor(props){
         super(props)
         this.state = {
-            firstname: "",
-            lastname: "",
-            bio: "",
-            age: 1,
-            isHappy: false 
+            firstname: props.user?.firstname || '',
+            lastname: props.user?.lastname || '',
+            bio: props.user?.bio || '',
+            age: props.user?.age || '',
+            isHappy: props.user?.isHappy || false,
         }
     }
     render(){
         return(
-            <form ref={(el) => this.myForm = el}>
-                <input placeholder="Ім'я" onChange={(e) => this.setState({firstname: e.target.value})} />
-                <input placeholder="Прізвище" onChange={(e) => this.setState({lastname: e.target.value})} />
-                <textarea placeholder="Біографія" onChange={(e) => this.setState({bio: e.target.value})} />
-                <input placeholder="Вік" onChange={(e) => this.setState({age: e.target.value})} />
+            <form>
+                <input value={this.state.firstname} placeholder="Ім'я" onChange={(e) => this.setState({firstname: e.target.value})} />
+                <input value={this.state.lastname} placeholder="Прізвище" onChange={(e) => this.setState({lastname: e.target.value})} />
+                <textarea value={this.state.bio} placeholder="Біографія" onChange={(e) => this.setState({bio: e.target.value})} />
+                <input value={this.state.age} placeholder="Вік" onChange={(e) => this.setState({age: e.target.value})} />
                 <label htmlFor="isHappy">"Ви щасливі?"</label>
-                <input type="checkbox" id="isHappy" className="checkbox_css" onChange={(e) => this.setState({isHappy: e.target.checked})} />
-                <button type="button" onClick={() => {
-                    this.myForm.reset()
-                    this.props.onAdd({
-                        firstname: this.state.firstname,
-                        lastname: this.state.lastname,
-                        bio: this.state.bio,
-                        age: this.state.age,
-                        isHappy: this.state.isHappy
-                })}
-                }>Додати</button>
+                <input checked={this.state.isHappy} type="checkbox" id="isHappy" className="checkbox_css" onChange={(e) => this.setState({isHappy: e.target.checked})} />
+                <button type="button" onClick={this.validate.bind(this)}>
+                    Додати
+                </button>
             </form>
         )
+    }
+    validate() {
+        this.userAdd = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            bio: this.state.bio,
+            age: this.state.age,
+            isHappy: this.state.isHappy,
+        }
+        if(this.props.user)
+            this.userAdd.id = this.props.user.id                    
+        this.props.onAdd(this.userAdd)
+        
+        this.setState({firstname: ''})
+        this.setState({lastname: ''})
+        this.setState({bio: ''})
+        this.setState({age: ''})
+        this.setState({isHappy: false})
     }
 }
 
