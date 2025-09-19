20ca1ceb664256361cc44c569447c7720aa77354
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-19 11:30:31 +0300

    added props to the component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,47 @@ import Users from "./components/Users"
 import AddUser from "./components/AddUser"
 
 class App extends React.Component {
-
+    constructor(props){
+        super(props)
+        this.state ={
+            users: [
+        {
+            id: 1,
+            firstname: "Kate",
+            lastname: "Bloom",
+            bio: "LoremLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            age: 40,
+            isHappy: true
+        },
+        {
+            id: 2,
+            firstname: "Lola",
+            lastname: "Black",
+            bio: "LoremLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            age: 28,
+            isHappy: false
+        }
+    ]
+        }
+        this.addUser = this.addUser.bind(this)
+    }
     render(){
         return (
             <div>
                 <Header title="Список користувачів"/>
                 <main>
-                    <Users />
+                    <Users users={this.state.users}/>
                 </main>
                 <aside>
-                    <AddUser />
+                    <AddUser onAdd={this.addUser}/>
                 </aside>        
             </div>
         )
+    }
+
+    addUser(user){
+        const id = this.state.users.length + 1
+        this.setState({users: [...this.state.users, {id, ...user}]})
     }
 
 }
