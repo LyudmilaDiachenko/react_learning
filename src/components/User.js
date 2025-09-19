2b469ee212029283e6500f1f4163363c83e8c91e
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-19 13:46:56 +0300

    added constructor & event on button

--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,29 @@
 import React from "react"
 import { CgCloseO } from "react-icons/cg";
 import { IoBuildSharp } from "react-icons/io5";
+import AddUser from "./AddUser";
 
 class  User extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            editForm: false
+        }
+}
     render(){
         return(
-            <div className="users">
-                <CgCloseO className="delete-icon" />
-                <IoBuildSharp className="edit-icon" />
+            <div className="user">
+                <CgCloseO onClick={() => this.props.onDelete(this.props.user.id)} 
+                    className="delete-icon" />
+                <IoBuildSharp onClick={() => this.setState({editForm: true})} 
+                    className="edit-icon" />
                 <h3>{this.props.user.firstname} {this.props.user.lastname}</h3>
                 <p>{this.props.user.bio}</p>
                 <p>{this.props.user.age} років</p>
                 <b>{this.props.user.isHappy ?  "Щасливий:)" : "Недуже щасливий:("}</b>
+
+                {this.state.editForm && <AddUser onAdd={this.props.onEdit}/>}
+
             </div>
         )
     }
