62cb9bc7180c64e2a3a5c3d4676daacd32e0c5ef
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-19 13:45:44 +0300

    form reset

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -13,20 +13,23 @@ class AddUser extends React.Component {
     }
     render(){
         return(
-            <form>
+            <form ref={(el) => this.myForm = el}>
                 <input placeholder="Ім'я" onChange={(e) => this.setState({firstname: e.target.value})} />
                 <input placeholder="Прізвище" onChange={(e) => this.setState({lastname: e.target.value})} />
                 <textarea placeholder="Біографія" onChange={(e) => this.setState({bio: e.target.value})} />
                 <input placeholder="Вік" onChange={(e) => this.setState({age: e.target.value})} />
                 <label htmlFor="isHappy">"Ви щасливі?"</label>
                 <input type="checkbox" id="isHappy" className="checkbox_css" onChange={(e) => this.setState({isHappy: e.target.checked})} />
-                <button type="button" onClick={() => this.props.onAdd({
-                    firstname: this.state.firstname,
-                    lastname: this.state.lastname,
-                    bio: this.state.bio,
-                    age: this.state.age,
-                    isHappy: this.state.isHappy
-                })}>Додати</button>
+                <button type="button" onClick={() => {
+                    this.myForm.reset()
+                    this.props.onAdd({
+                        firstname: this.state.firstname,
+                        lastname: this.state.lastname,
+                        bio: this.state.bio,
+                        age: this.state.age,
+                        isHappy: this.state.isHappy
+                })}
+                }>Додати</button>
             </form>
         )
     }
